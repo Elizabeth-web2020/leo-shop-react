841c71002b8987b8b001765dfc2abf2f6dddaf86
Elizabeth Popova <elizabethpopova2020@gmail.com>
2021-12-12 01:51:53 +0100

    added some styles in ItemDetails file

--- src/views/ItemDetails.js
+++ src/views/ItemDetails.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 function ItemDetails() {
     const {itemId} = useParams();
+
+    const [data, setData] = useState([]);
+
+    useEffect( () => {
+       fetch(`https://fakestoreapi.com/products/${itemId}`)
+        .then(res=>res.json())
+        .then(res => {setData(res); console.log(res)})
+    }, [])
+
+    const {title, price, image, description} = data;
+
         return (
-            <div>
-                <Link to='/catalog'>Back</Link>
-                <h1>Item #{itemId}</h1>
+            <div className="item-details-container">
+                <Link to='/catalog' className="back-btn">Back</Link>
+                <div className="item-cart-content">
+                    <h1 className="item-title">Item #{itemId} - {title}</h1>
+                    <img className="item-image" src={image} alt="image" />
+                    <div className="item-price">{price} $</div>
+                    <p className="item-description">{description}</p>
+                </div>
             </div>
         );
 }
